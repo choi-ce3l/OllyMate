bcbb5bfe232655869dc941d199f8c69d4003c256
choi-ce3l <116821422+choi-ce3l@users.noreply.github.com>
2024-11-12 23:35:18 +0900

    [style] code format changes

--- preprocessing.py
+++ preprocessing.py
@@ -1,40 +1,27 @@
 import pandas as pd
 import ast
 
+
 def preprocessing(file_path):
     df = pd.read_csv(file_path)
-    # skintype, skinton, skinconcerns 중 하나라도 Nan이면 해당 행 제거
-    df = df.dropna(subset=['skintype', 'skinton', 'skinconcerns'])
-    df.reset_index(drop=True, inplace=True)
+
+    # NaN값 있는 행 제거
+    df.dropna(subset=['skintype', 'skinton', 'skinconcerns'], inplace=True)
 
     # 가격 데이터 전처리
     df['price'] = df['price'].str.replace(',', '').astype(int)
 
-    # 가격 범주 설정
+    # 가격 범주 설정 및 가격 카테고리 생성
     bins = [0, 20000, 30000, 50000, float('inf')]
     labels = ['2만원이하', '2-3', '3-5', '5만원이상']
+    df['price_category'] = pd.cut(df['price'], bins=bins, labels=labels)
 
-    # 가격 카테고리 생성
-    df['price_category'] = pd.cut(df['price'], bins=bins, labels=labels).astype('object')
+    # 리스트를 문자열로 변환
+    for col in ['skintype', 'skinton', 'skinconcerns']:
+        df[col] = df[col].apply(ast.literal_eval).apply(' '.join)
 
-    # 예비 데이터 생성
-    processed_df = pd.DataFrame(df)
-
-    # feature를 합치기 위해 []지우고 문자열로 반환
-    processed_df['skintype'] = processed_df['skintype'].apply(ast.literal_eval)
-    processed_df['skintype'] = processed_df['skintype'].apply(lambda x: ' '.join(x))
-    processed_df['skinton'] = processed_df['skinton'].apply(ast.literal_eval)
-    processed_df['skinton'] = processed_df['skinton'].apply(lambda x: ' '.join(x))
-    processed_df['skinconcerns'] = processed_df['skinconcerns'].apply(ast.literal_eval)
-    processed_df['skinconcerns'] = processed_df['skinconcerns'].apply(lambda x: ' '.join(x))
-
-    for i in range(len(processed_df)):
-        if (len(processed_df['skintype'][i]) == 0 or len(processed_df['skinton'][i]) == 0 or len(
-                processed_df['skinconcerns'][i]) == 0):
-            processed_df.drop([i], inplace=True)
-        else:
-            continue
-
-    processed_df.reset_index(drop=True, inplace=True)
+    # 모든 컬럼에 빈 문자열이 없는 행만 필터링
+    mask = (df['skintype'] != '') & (df['skinton'] != '') & (df['skinconcerns'] != '')
+    processed_df = df[mask].reset_index(drop=True)
 
     return processed_df
