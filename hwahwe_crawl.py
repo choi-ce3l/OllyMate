63327d68e6c870ecb587310cef4b4a2eb721714a
MainHHH <bmn0330@gmail.com>
2024-11-17 22:00:42 +0900

    feat: - 화해 상품 페이지에서 feature 가져오는 부분 selenium으로 교체 - 쿠키를 항상 바꿔줘야 되는 문제와 여러번 request 시 접근 제한 문제 해결

--- hwahwe_crawl.py
+++ hwahwe_crawl.py
@@ -1,9 +1,13 @@
 import re
-import sys
 import requests
 import pandas as pd
+import sys
+import time
 from bs4 import BeautifulSoup
-from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 # csv 파일명 입력
@@ -12,10 +16,9 @@ def hwahae_crawl(goodsData):
     df = pd.read_csv(goodsData, encoding='utf-8')
 
     # 상품명 추출
-    product_name = list(df['product_name'].unique())
+    product_name = list(df['goodsName'].unique())
 
     data = []
-    glob_feature = []
     for name in product_name:
         try:
             # 화해에 검색 할 수 있게 상품명 처리
@@ -38,28 +41,41 @@ def hwahae_crawl(goodsData):
             # 상품 url
             product_url = f'https://www.hwahae.co.kr/{href}'
 
-            # cookie 사용 할 때 마다 바꿔야함
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
-                'Cookie': 'CHECK_IS_REPRESENT_GOODS_35549=true; aws-waf-token=00fb4178-33de-47bb-819b-20aad2355258:AQoAZ6IH1LkCAQAA:GQMLwClW8rADSnahAXndE8wpq5WOzf+gyULse5eqQ4nO308BVUp69L/Lt5arW8Nymv3HncPmF617ELS9XBT+dwC1nIBhOUCZEd7wRUXtu5DzKEqfvhyAwzCwb/XNI4jXNvdcR+E/BbFiFqs8nDv9xRmMtQ2nrASHu+G9x2CSai6Qq7ZzUeYihsGP6+A/C43dhl1B'
-            }
-
             # feature 가져오기
-            res2 = requests.get(product_url, headers=headers)
-            soup2 = BeautifulSoup(res2.text, 'html.parser')
-
-            print("res2", res2)
-
-            li = soup2.select('div.flex.justify-between.px-20.my-24 > div.grow.mr-24.w-1\/2 > ul > li')
+            # 크롬 드라이버 설정
+            service = Service(ChromeDriverManager().install())
+            options = webdriver.ChromeOptions()
+            options.add_argument('disable-blink-features=AutomationControlled')  # 자동화 탐지 방지
+            options.add_argument("--headless")  # 헤드리스 모드로 실행
+            driver = webdriver.Chrome(service=service, options=options)
 
             features = []
             function = []
             formulation = []
 
+            try:
+                # 페이지로 이동
+                driver.get(product_url)
+                time.sleep(1)  # 페이지 로딩 대기
+
+                # feature kewword 추출
+                li = driver.find_elements(By.CSS_SELECTOR,
+                                          'div.flex.justify-between.px-20.my-24 > div.grow.mr-24.w-1\/2 > ul > li ')
+
+                for span in li:
+                    span_tag = span.find_element(By.CSS_SELECTOR, 'span').text
+                    features.append(span_tag)
+                    print(span_tag)
+
+
+            finally:
+                # 드라이버 종료
+                driver.quit()
+
             # 수분
             moisture = [
                 "수분있는", "보습잘되는", "속건조에효과있는",
-                "흡수잘되는", "겉돌지않는", "촉촉한"
+                "흡수잘되는", "겉돌지않는"
             ]
 
             # 진정
@@ -72,7 +88,7 @@ def hwahae_crawl(goodsData):
 
             # 미백
             whitening = [
-                "미백효과가있는", "브라이트닝효과있는",
+                "미백효과가있는", "브리이트닝효과있는",
                 "안색이개선되는", "피부톤이개선되는",
                 "톤업되는", "맑은"
             ]
@@ -80,27 +96,23 @@ def hwahae_crawl(goodsData):
             # 모공
             elasticity = [
                 "모공관리되는", "피지없어지는", "블랙헤드없어지는",
-                "각질제거잘되는", "노폐물제거되는"
+                "각질제거잘되는", "노폐물제거되는", "화이트헤드없어지는"
             ]
 
             # 가벼운 제형
             light_formulation = [
                 "리치하지않은", "가벼운", "산뜻한",
                 "흘러내리지않는", "끈적하지않은",
-                "답답하지않은", "얇게발리는", "밀림없는"
+                "답답하지않은", "얇게발리는", "밀림없는",
+                "매트하지않은"
             ]
 
             # 무거운 제형
             heavy_formulation = [
                 "리치한", "쫀득한", "지속력좋은",
-                "겨울에사용하기좋은", "윤기나는", "제형묽지않은"
+                "겨울에사용하기좋은", "윤기나는", "제형묽지않은",
+                "매끄러운"
             ]
-
-            for span in li:
-                span_tag = span.select_one('span').text
-                features.append(span_tag)
-                glob_feature.append(span_tag)
-                print(span_tag)
 
             if any(keyword in features for keyword in moisture):
                 function.append("수분")
@@ -116,21 +128,18 @@ def hwahae_crawl(goodsData):
                 formulation.append("무거운 제형")
 
             name_feature = {
-                'product_name': name,
+                'goodsName': name,
                 "function": function,
                 "formulation": formulation
             }
 
             data.append(name_feature)
 
-            # 접근 제한 방지를 위한 sleep
-            sleep(2)
 
         except:
             print("keyword error")
-            sleep(2)
 
-    file_name = goodsData.replace(".csv", "_fin.csv")
+    file_name = goodsData.replace(".csv", "_fin_after.csv")
 
     new_df = pd.DataFrame(data)
     new_df.to_csv(file_name, encoding='utf-8-sig', index=False)
