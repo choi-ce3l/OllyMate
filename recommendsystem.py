af8d920e0c144282b459c500456f19e15057397b
choi-ce3l <116821422+choi-ce3l@users.noreply.github.com>
2024-11-21 18:20:54 +0900

    [style] change variables

--- recommendsystem.py
+++ recommendsystem.py
@@ -1,12 +1,12 @@
 from preprocessing import *
-from filtering import *
+from contentfiltering import *
 import pandas as pd
-
 
 class RecommendSystem:
     def __init__(self, file_path):
         # Load data and initialize the recommender
-        self.df = preprocessing(file_path)
+        # self.df = preprocessing(file_path) # 전처리가 필요한 경우 사용
+        self.df=pd.read_csv(file_path) # 전처리가 필요없는 경우 사용
         self.recommender = FastSkincareRecommender(self.df)
 
     def recommend_existing_product(self, idx):
@@ -16,7 +16,7 @@ class RecommendSystem:
         :param idx: Index of the existing product in the dataset.
         :return: List of recommendations with product names and similarity scores.
         """
-        recommendations = self.recommender.fit_and_recommend(item_idx=idx, n_recommendations=3)
+        recommendations = self.recommender.fit_and_recommend(item_idx=idx, n_recommendations=6)
 
         print("Recommendations based on existing product:")
         for product_name, similarity in recommendations:
@@ -25,14 +25,14 @@ class RecommendSystem:
         return recommendations
 
     def recommend_new_user_profile(self, skintype='복합성', skintone='가을웜톤', skinconcern='트러블 모공',
-                                   price_category='2-3', category='스킨/토너', function='수분', formulation='가벼운 제형'):
+                                   pricerange='2-3', category='스킨/토너', function='수분', formulation='가벼운 제형'):
         """
         Get recommendations based on a new user profile.
 
         :param skintype: User's skin type (default is '복합성').
         :param skintone: User's skin tone (default is '가을웜톤').
         :param skinconcern: User's skin concerns (default is '트러블 모공').
-        :param price_category: User's preferred price category (default is '2-3').
+        :param pricerange: User's preferred price category (default is '2-3').
         :param category: Product category (default is '스킨/토너').
         :param function: Desired product function (default is '수분').
         :param formulation: Preferred formulation (default is '가벼운 제형').
@@ -40,16 +40,27 @@ class RecommendSystem:
         """
         # Define the user profile data
         user_profile = [{'skintype': skintype, 'skintone': skintone, 'skinconcern': skinconcern,
-                         'price_category': price_category, 'category': category,
+                         'pricerange': pricerange, 'category': category,
                          'function': function, 'formulation': formulation}]
 
         # Convert to DataFrame
         user_profile_df = pd.DataFrame(user_profile)
 
-        recommendations = self.recommender.fit_and_recommend(new_data=user_profile_df, n_recommendations=3)
+        recommendations = self.recommender.fit_and_recommend(new_data=user_profile_df, n_recommendations=6)
 
         print("Recommendations based on new user profile:")
         for product_name, similarity in recommendations:
             print(f"Product: {product_name}, Similarity: {similarity:.4f}")
 
         return recommendations
+
+
+'''
+사용방법
+from recommendsystem import *
+
+file_path='data/final_data.csv'
+system=RecommendSystem(file_path)
+# existing_recommendations=system.recommend_existing_product(idx=0)
+newuser_recommendations=system.recommend_new_user_profile()
+'''
