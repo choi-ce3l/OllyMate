cf4d1fffb3356376ee056d0d401148643239cb68
MainHHH <bmn0330@gmail.com>
2024-11-25 16:44:28 +0900

    [fix] DB 모든 정보 불러와 dataframe 만드는 함수 삭제, search_db 함수의 반환되는 값 goodsNo 추가

--- search_db.py
+++ search_db.py
@@ -27,7 +27,7 @@ def search_db(product_list):
         with connection.cursor() as cursor:
             result = []
             for i in product_list:
-                query = f"select goodsName, purchase_link, image_link, price, `function`, formulation from cosmetic_data where goodsNo = '{i}';"
+                query = f"select goodsName, purchase_link, image_link, price, `function`, formulation, goodsNo from cosmetic_data where goodsName = '{i}';"
                 cursor.execute(query)
                 fetch = cursor.fetchone()
                 fetch = {
@@ -36,7 +36,8 @@ def search_db(product_list):
                     "image_link": fetch[2],
                     "price": fetch[3],
                     "function": fetch[4],
-                    "formulation": fetch[5]
+                    "formulation": fetch[5],
+                    "goodsNo": fetch[6]
                 }
                 result.append(fetch)
 
@@ -45,32 +46,3 @@ def search_db(product_list):
         connection.close()
 
     return result
-
-
-def mk_df():
-    load_dotenv()
-
-    # 데이터베이스 연결에 필요한 정보들 저장
-    host = os.getenv("DB_HOST")
-    port = os.getenv("DB_PORT")
-    user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWD")
-    database = os.getenv("DB_NAME")
-
-    con = pymysql.connect(
-        host=host,             # 예: "localhost"
-        port=int(port),        # MySQL 포트 넘버
-        user=user,             # MySQL 사용자 이름
-        password=password,     # MySQL 비밀번호
-        database=database,     # MySQL 데이터베이스 이름
-        charset="utf8mb4"      # 문자 인코딩
-    )
-
-    query = 'select * from cosmetic_data;'
-
-    df = pd.read_sql(query, con)
-
-    # 연결 종료
-    con.close()
-
-    return df
